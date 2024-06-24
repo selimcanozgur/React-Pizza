01a808790a8ed8232b9e433d10c9ad7681f945a7
Selimcan <webisdeveloper@gmail.com>
2024-06-24 14:33:12 +0300

    new router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import Cart from "./features/cart/Cart";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/menu/:orderId",
+        element: <Order />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
